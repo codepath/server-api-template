88799acea4cd836eec7a4d4f31a76281f1376ba6
Nathan Esquenazi <nesquena@gmail.com>
2013-03-17 22:54:24 -0700

    Adds devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # https://github.com/plataformatec/devise
+  # before_filter :authenticate_user!
+  # user_signed_in?
+  # current_user
 end
