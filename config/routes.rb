d578cfdc99d213e633e720dd41d0492ee26685a6
Nathan Esquenazi <nesquena@gmail.com>
2013-03-17 22:56:32 -0700

    Adds rails_admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ServerApiTemplate::Application.routes.draw do
   devise_for :users
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318055154) do
+ActiveRecord::Schema.define(:version => 20130318055526) do
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
