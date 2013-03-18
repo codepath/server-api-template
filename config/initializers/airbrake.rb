526b54bac011cda55436e47fe741cacc68924b85
Nathan Esquenazi <nesquena@gmail.com>
2013-03-17 22:54:51 -0700

    Adds airbrake placeholder

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,4 @@
-Airbrake.configure do |config|
-  config.api_key = 'ba95f75bb179739c92f40525cdddc4ef'
-end
+# TODO configure airbrake for error handling
+# Airbrake.configure do |config|
+#  config.api_key = 'my_token_here'
+# end
